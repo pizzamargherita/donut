d37807f5169688f3e79c97d6cf454d1e548227cd
Adrin <machadoadrin@gmail.com>
2021-03-27 13:53:55 -0400

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//goey menu
+//GOEY MEN
 
 $(window).load(function(){
 	var height = window.innerHeight,
@@ -83,3 +83,59 @@ $(window).load(function(){
 	window.requestAnimationFrame(svgCurve);
 	
 });
+
+//SLIDES ON BLOCK 2
+  
+
+$(document).ready(function() {
+	var getslideHeight = $('.slide.active').height();
+  
+	$('.slides').css({
+	  height: getslideHeight
+	});
+  
+	function calcslideHeight() {
+	  getslideHeight = $('.slide.active').height();
+  
+	  $('.slides').css({
+		height: getslideHeight
+	  });
+	}
+
+	var slideItem = $('.slide'),
+	  slideCurrentItem = slideItem.filter('active');
+  
+	$('#next').on('click', function(e) {
+	  e.preventDefault();
+  
+	  var nextItem = slideCurrentItem.next();
+  
+	  slideCurrentItem.removeClass('active');
+  
+	  if (nextItem.length) {
+  
+		slideCurrentItem = nextItem.addClass('active');
+	  } else {
+		slideCurrentItem = slideItem.first().addClass('active');
+	  }
+  
+	  calcslideHeight();
+	});
+  
+	$('#prev').on('click', function(e) {
+	  e.preventDefault();
+  
+	  var prevItem = slideCurrentItem.prev();
+  
+	  slideCurrentItem.removeClass('active');
+  
+	  if (prevItem.length) {
+		slideCurrentItem = prevItem.addClass('active');
+	  } else {
+		slideCurrentItem = slideItem.last().addClass('active');
+	  }
+  
+	  calcslideHeight();
+	});
+  });
+  //SLIDES ON BLOCK 2 END
